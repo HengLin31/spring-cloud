e53ac7fdaed61983f221004f3b3b20eb0f62d822
Heng Lin <enjoymycodinglife@gmail.com>
2019-11-10 18:44:05 +0800

    add eureka module, it's Client-side service discovery like zookeeper

---
+++ eureka/src/main/java/pers/henglin/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package pers.henglin.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by linheng on 10/11/2019.
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
